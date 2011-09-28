ed71bc3a6673e7c96c1c3b7db450068de0df7c85
mpermar <mpermar@gmail.com>
2011-09-28 17:36:36 +0200

    removed unexistent method

--- src/test/java/com/voxeo/rayo/client/test/DialTest.java
+++ src/test/java/com/voxeo/rayo/client/test/DialTest.java
@@ -16,16 +16,5 @@ public class DialTest extends XmppIntegrationTest {
 		
 		Thread.sleep(400);
 		assertServerReceived("<iq id=\"*\" type=\"set\" from=\"userc@localhost/voxeo\" to=\"localhost\"><dial xmlns=\"urn:xmpp:rayo:1\" to=\"tel:123456789\" from=\"sip:userc@127.0.0.1:5060\"></dial></iq>");
-	}
-	
-	@Test
-	public void testDialText() throws Exception {
-		
-		rayo.answer(lastCallId);
-		rayo.dial("tel:123456789");
-		
-		Thread.sleep(400);
-		assertServerReceived("<iq id=\"*\" type=\"set\" from=\"userc@localhost/voxeo\" to=\"localhost\"><dial xmlns=\"urn:xmpp:rayo:1\" to=\"tel:123456789\" from=\"sip:userc@127.0.0.1:5060\"></dial></iq>");
-	}
-	
+	}	
 }

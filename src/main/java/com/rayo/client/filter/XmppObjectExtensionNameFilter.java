7d025201c6866630cb144ae9ac8c0b5e7ae7744d
mpermar <mpermar@gmail.com>
2011-11-16 18:21:40 +0100

    new constructor

--- src/main/java/com/rayo/client/filter/XmppObjectExtensionNameFilter.java
+++ src/main/java/com/rayo/client/filter/XmppObjectExtensionNameFilter.java
@@ -6,10 +6,18 @@ import com.rayo.client.xmpp.stanza.Stanza;
 public class XmppObjectExtensionNameFilter extends AbstractXmppObjectFilter {
 
 	private String name;
+	private String from;
 	
 	public XmppObjectExtensionNameFilter(String name) {
 		
 		this.name = name;
+	}
+
+	
+	public XmppObjectExtensionNameFilter(String name, String from) {
+		
+		this.name = name;
+		this.from = from;
 	}
 	
 	@SuppressWarnings("rawtypes")
@@ -20,13 +28,25 @@ public class XmppObjectExtensionNameFilter extends AbstractXmppObjectFilter {
 			Stanza stanza = (Stanza)object;
 			if (stanza.hasExtension()) {
 				if (name.equalsIgnoreCase(stanza.getExtension().getStanzaName())) {
-					return stanza.getExtension();
+					if (from != null) {
+						if (stanza.getFrom().equals(from)) {
+							return object;
+						}
+					} else {
+						return object;
+					}
 				}
 			} else {
 				if (stanza.getChildName() != null) {
 					// will still handle default stanzas as extensions for the sake of this filter
 					if (name.equalsIgnoreCase(stanza.getChildName())) {
-						return stanza.getExtension();
+						if (from != null) {
+							if (stanza.getFrom().equals(from)) {
+								return object;
+							}
+						} else {
+							return object;
+						}
 					}
 				}
 			}
